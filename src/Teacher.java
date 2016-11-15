1b2f54a41739890a6d056ea790f5986c43414498
chalitphol kuensuwan <chalitphol@gmail.com>
2016-11-15 14:30:55 +0700

    gg

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,3 @@
 public class Teacher extends User {
+
 }
--- src/User.java
+++ src/User.java
@@ -1,26 +1,67 @@
 public class User implements IUser {
+
+    protected String name;
+    protected String password;
+    protected int type;
+
+    User(int type, String name, String password){
+
+        setType(type);
+        setName(name);
+        setPassword(password);
+    }
+
     @Override
     public String getName() {
-        return null;
+
+        if(this.name == ""){
+            throw new RuntimeException("Name is empty. Please set your name.");
+        }
+        return this.name;
     }
 
     @Override
     public String setName(String name) {
-        return null;
+
+        if(name == "" ){
+           throw new RuntimeException("Please input your name.");
+       }
+        this.name = name;
+        return this.name;
     }
 
     @Override
     public String setPassword(String password) {
-        return null;
+
+        if(password == ""){
+            throw new RuntimeException("Please input your password.");
+        }
+        this.password = password;
+        return this.password;
     }
 
     @Override
     public boolean isPasswordCorrect(String password) {
-        return false;
+
+        if(password == this.password){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     @Override
     public int getType() {
-        return 0;
+        return type;
+    }
+
+    @Override
+    public int setType(int type) {
+        if(type != 1 && type != 2){
+            throw new RuntimeException("Your type is wrong.");
+        }
+        this.type = type;
+        return  this.type;
     }
 }
