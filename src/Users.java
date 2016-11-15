8237a5545a5843c7dfa70c4f6413663b1fe788a2
Toufu <timecrisis1995@live.com>
2016-11-15 15:02:39 +0700

    Users done?

--- src/Users.java
+++ src/Users.java
@@ -5,34 +5,61 @@ public class Users {
     private ArrayList<IUser> userList;
 
     // Create new user with type, name and password
+    public Users ()
+    {
+        userList = new ArrayList<IUser>();
+    }
     // user created with this method should be automatically added to userList;
     public IUser create(int type, String name, String password) {
-        return null;
+
+        IUser newuser = new User(type,name,password);
+        return newuser;
     }
 
     // Add new user to repository
     public void add(IUser user) {
+        userList.add(user);
+    }
+
+    public void add(int type,String username , String password)
+    {
+        User newuser = new User(type , username , password);
+        userList.add(newuser);
 
     }
 
     // Delete user from repository
     // Throw  RuntimeException if the user is not in the list
     public void remove(IUser user) throws RuntimeException {
-
+        if(userList.size() < 1 )
+            throw new RuntimeException("Empty User in list");
+        userList.remove(user);
     }
 
     // Return true if the user is in the list
-    public boolean exists(IUser user) {
+    public boolean exists(IUser user)
+    {
+        for(IUser users : userList)
+        {
+            if(users.getName().equals(user))
+                return true;
+        }
         return false;
     }
 
     // Return number of user in the list
     public int count() {
-        return 0;
+        return userList.size();
     }
 
     // Return number of user in the list, according to type
-    public int countByType(int type) {
-        return 0;
+    public int countByType(int type)
+    {
+        int count=0;
+        for (IUser users : userList)
+        {
+            if(users.getType() == type) count++;
+        }
+        return count;
     }
 }
